d5abe9774a53456dfb19020ae70d4c6208f5cb21
theo declerck <theo.declerck@epita.com>
2022-01-25 15:42:32 +0100

    Partie 7 - Persistence en base de données

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -2,16 +2,24 @@ package fr.lernejo.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoListController {
+
+    private final TodoRepository repository;
+
+    public TodoListController(TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+
     @GetMapping("/api/todo")
-    public Todo get(){
-        return new Todo("valou gros pd", "theo");
+    public Iterable<TodoEntity> get(){
+        return this.repository.findAll();
     }
     @PostMapping("/api/todo")
-    public void post(){
-
+    public void post(@RequestBody TodoEntity todoEntity){
+        this.repository.save(todoEntity);
     }
 }
